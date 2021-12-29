fec812d26fc264cac39f2e81eb4ef40c3e008d47
Kalpana Jasoliya <kalpana.jasoliya@bacancy.com>
2021-12-29 10:16:05 +0530

    Initial Commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Expenses.css'
+import ExpensesList from './ExpensesList'
+import ExpensesFilter from './ExpensesFilter'
+import Card from '../UI/Card'
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+  
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses}/>
+    </Card>
+  )
+}
+
+export default Expenses
